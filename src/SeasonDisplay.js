0ce2c82f217cbf100133fa5af3539002579f3105
Patrick Brito <patrickbrito95@gmail.com>
2021-03-11 16:47:43 -0300

    commited

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <h1>hello!</h1>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat < 0 ? "Summer" : "Winter";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const iconName = season === "Summer" ? "sun" : "snowflake";
+
+  return (
+    <h1>
+      {season === "Summer" ? (
+        <div>
+          <i className={`${iconName} icon`} />
+          <h1>Bora para praia!!!</h1>
+          <i className={`${iconName} icon`} />
+        </div>
+      ) : (
+        <div>
+          <i className={`${iconName} icon`} />
+          <h1>Ixii, ta frio. Quero uma Sopa</h1>
+          <i className={`${iconName} icon`} />
+        </div>
+      )}
+    </h1>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // Aqui inicializamos o State com algum valor, que no caso aqui é "Null"
+    // Aqui inicializamos o State com algum valor, que no caso aqui é Null/""
     // Aqui temos uma exceção que podemos atribuir o 'this.state' a outro valor!!
     this.state = { lat: null, errMessage: "" };
+  }
 
+  // state = { lat: null, errMessage: ''}; <-- Essa forma tem o mesmo efeito da função construtora acima, pode substituí-la sem problemas.
+
+  // ComponentDidMount é chamado automaticamente quando o componente é renderizado na tela (Bom lugar para carregamento inicial de dados)
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // Aqui atualizamos o State buscando o valor dentro do objeto
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errMessage: err.message });
-      }
+      // Aqui atualizamos o State buscando o valor dentro do objeto
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
     );
+  }
+  // ComponentDidUpdate é chamado quando há atualização no State, e ele é rerenderizado (Bom lugar para carregamento de dado quando State/Props atualizam)
+  componentDidUpdate() {
+    console.log("My component was just updated - it rerendered!");
   }
 
   // O React diz: Precisamos definir o 'render()'!!
@@ -26,7 +32,7 @@ class App extends React.Component {
     }
 
     if (this.state.lat && !this.state.errMessage) {
-      return <h1>Latitude: {this.state.lat}</h1>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     if (!this.state.lat && !this.state.errMessage) {
